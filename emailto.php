ed8bf3419dbf65e3e1a9cc1f9d79f2af585a6eca
GABRIEL BARBOSA <gabriellb438@outlook.com>
2021-01-01 19:40:17 -0300

    Validação dos dados

--- emailto.php
+++ emailto.php
@@ -1,25 +1,29 @@
 <?php
 // var_dump($_POST['p2']);
-$to = 'orbt2001@gmail.com';
-$subject = '[CONTATO] '.$_POST['p3'];
-$html = "
-<html>
-<body>
-<div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
-	<img src='https://orbt.com.br/orbt/img/orbtwh.svg' width='100'>
-</div>
-<div style='text-align:center; margin: 50px 10px;'>
-".$_POST['p1']." entrou em contato através do site: <br>".$_POST['p4']."
-<br> Email para contato:".$_POST['p2']."
-</div>
-<div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
-&nbsp;
-</div>
-</body>
-</html>";
-$headers = 'From: '.$_POST['p2'].'\r\n'.
-    'Content-type: text/html; charset=UTF-8';
+if($_POST['p1']==''||$_POST['p2']==''||$_POST['p3']=''||$_POST['p4']==''){
+    return false;
+}else{
+    $to = 'orbt2001@gmail.com';
+    $subject = '[CONTATO] '.$_POST['p3'];
+    $html = "
+    <html>
+    <body>
+    <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
+        <img src='https://orbt.com.br/orbt/img/orbtwh.svg' width='100'>
+    </div>
+    <div style='text-align:center; margin: 50px 10px;'>
+    ".$_POST['p1']." entrou em contato através do site: <br>".$_POST['p4']."
+    <br> Email para contato:".$_POST['p2']."
+    </div>
+    <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
+    &nbsp;
+    </div>
+    </body>
+    </html>";
+    $headers = 'From: '.$_POST['p2'].'\r\n'.
+        'Content-type: text/html; charset=UTF-8';
 
-$ret = mail($to, $subject, $html, $headers);
-return $ret;
+    $ret = mail($to, $subject, $html, $headers);
+    return $ret;
+}
 ?>
