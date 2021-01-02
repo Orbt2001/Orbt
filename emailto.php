1f9df879405f57ca242e35f5de5e80335e16546f
ABNER ANDRE <abnerndr@gmail.com>
2021-01-02 11:54:59 -0300

    updater

--- emailto.php
+++ emailto.php
@@ -3,7 +3,7 @@
 
     $to = 'orbt2001@gmail.com';
     $subject = '[CONTATO] '.$_POST['p3'];
-    $body = "<html>
+    $html = "<html>
     <body>
     <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
     <img src='https://orbt.com.br/orbt/img/orbtwh.svg' width='100'>
@@ -18,6 +18,6 @@
         </html>";
     $headers = 'From: '.$_POST['p2'].'\r\n'.'Content-type: text/html; charset=UTF-8';
 
-    mail($to, $subject, $body);
+    mail($to, $subject, $html);
     return true;
 ?>
