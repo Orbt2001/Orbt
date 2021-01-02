406e5806b011ccb8efd3991c76fc96069c2dd0ad
ABNER ANDRE <abnerndr@gmail.com>
2021-01-02 11:57:01 -0300

    update header

--- emailto.php
+++ emailto.php
@@ -16,8 +16,9 @@
         </div>
         </body>
         </html>";
-    $headers = 'From: '.$_POST['p2'].'\r\n'.'Content-type: text/html; charset=UTF-8';
+        
+    $headers='Content-Type: text/html; charset="utf-8"';
 
-    mail($to, $subject, $html);
+    mail($to, $subject, $headers, $html);
     return true;
 ?>
