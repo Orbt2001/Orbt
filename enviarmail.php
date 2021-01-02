89edd6edbcdd5d686e6b00f7893f3a980b326279
GABRIEL BARBOSA <gabriellb438@outlook.com>
2021-01-02 15:31:06 -0300

    email update

--- enviarmail.php
+++ enviarmail.php
@@ -11,7 +11,7 @@ $body = "
 
     <div style='text-align:center; margin: 50px 10px;'>
     ".$_POST['p1']." entrou em contato através do site: <br>".$_POST['p4']."
-    <br> Email para contato:.$_POST['p2'].
+    <br> Email para contato:".$_POST['p2']."
         </div>
     
     <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
@@ -23,6 +23,6 @@ $body = "
 $headers = 'Content-Type: text/html; charset=utf-8';
 
 
-mail($to, $subject, $body);
-return true;
+$ret = mail($to, $subject, $body);
+return $ret;
 ?>
