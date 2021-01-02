ebda2a1d22453aff528bfb8f593041f2c4de8227
ABNER ANDRE <abnerndr@gmail.com>
2021-01-02 12:05:45 -0300

    update !

--- emailto.php
+++ emailto.php
@@ -9,10 +9,11 @@
     <img src='https://orbt.com.br/orbt/img/orbtwh.svg' width='100'>
     </div>
     
-    '<div style='text-align:center; margin: 50px 10px;'>
-    .$_POST['t1']. entrou em contato através do site: <br>.$_POST['t4'].
-    <br> Email para contato:.$_POST['t2'].
-    </div>'
+    <div style='text-align:center; margin: 50px 10px;'>
+    ".$_POST['t1']" entrou em contato através do site: <br>".$_POST['t4']"
+    <br> Email para contato:".$_POST['t2']"
+    </div>
+
         
     <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
     &nbsp;
