0a8174aa45b7f543e54f0e0b6d6627340e74eeec
GABRIEL BARBOSA <gabriellb438@outlook.com>
2021-01-02 15:49:07 -0300

    charset update utf-8

--- enviarmail.php
+++ enviarmail.php
@@ -21,7 +21,7 @@ $body = "
 </html>";
 
 $headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 $headers .= 'From: '.$_POST['p3'].' <'.$_POST['p2'].'>';
 
 $ret = mail($to, $subject, $body, $headers);
