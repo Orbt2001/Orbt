df02bf63cd9a64e3e5760590301b8308db0808ec
ABNER ANDRE <abnerndr@gmail.com>
2021-01-02 11:43:53 -0300

    $body update

--- emailto.php
+++ emailto.php
@@ -3,21 +3,21 @@
 
     $to = 'orbt2001@gmail.com';
     $subject = '[CONTATO] '.$_POST['p3'];
-    $body ="
-    <html>
-    <body>
+    $body = <body>
     <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
-        <img src='https://orbt.com.br/orbt/img/orbtwh.svg' width='100'>
-    </div>
-    <div style='text-align:center; margin: 50px 10px;'>
-    ".$_POST['p1']." entrou em contato através do site: <br>".$_POST['p4']."
-    <br> Email para contato:".$_POST['p2']."
-    </div>
-    <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
-    &nbsp;
-    </div>
+            <img src='https://orbt.com.br/orbt/img/orbtwh.svg' width='100'>
+        </div>
+    
+        <div style='text-align:center; margin: 50px 10px;'>
+        ".$_POST['p1']." entrou em contato através do site: <br>".$_POST['p4']."
+        <br> Email para contato:".$_POST['p2']."
+        </div>
+        
+        <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
+        &nbsp;
+        </div>
     </body>
-    </html>";
+    </html>;
     $headers = 'From: '.$_POST['p2'].'\r\n'.'Content-type: text/html; charset=UTF-8';
 
     mail($to,$subject,$body,$headers);
--- teste.php
+++ teste.php
@@ -1,14 +1,22 @@
-<html>
-    <body>
-    <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
         <img src='https://orbt.com.br/orbt/img/orbtwh.svg' width='100'>
     </div>
+
     <div style='text-align:center; margin: 50px 10px;'>
     ".$_POST['p1']." entrou em contato através do site: <br>".$_POST['p4']."
     <br> Email para contato:".$_POST['p2']."
     </div>
+    
     <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
     &nbsp;
     </div>
-    </body>
-    </html>"
+</body>
+</html>
