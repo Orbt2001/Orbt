460690d75f5184d6b2a43b5462560b11c88ad11a
ABNER ANDRE <abnerndr@gmail.com>
2021-01-02 11:41:21 -0300

    update teste.php

---
+++ teste.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+    <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
+        <img src='https://orbt.com.br/orbt/img/orbtwh.svg' width='100'>
+    </div>
+    <div style='text-align:center; margin: 50px 10px;'>
+    ".$_POST['p1']." entrou em contato através do site: <br>".$_POST['p4']."
+    <br> Email para contato:".$_POST['p2']."
+    </div>
+    <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
+    &nbsp;
+    </div>
+    </body>
+    </html>"
