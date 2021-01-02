83e11e5e9b5870c73b3ff8db3e4cbd0eeb2692a5
ABNER ANDRE <abnerndr@gmail.com>
2021-01-02 11:51:38 -0300

    update echo

--- emailto.php
+++ emailto.php
@@ -6,17 +6,17 @@
     $body = echo <html>
     echo <body>
     echo <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
-            <img src='https://orbt.com.br/orbt/img/orbtwh.svg' width='100'>
-        </div>
+    echo <img src='https://orbt.com.br/orbt/img/orbtwh.svg' width='100'>
+    echo  </div>
     
         echo  <div style='text-align:center; margin: 50px 10px;'>
         ".$_POST['p1']." entrou em contato através do site: <br>".$_POST['p4']."
-        <br> Email para contato:".$_POST['p2']."
-        </div>
+        echo <br> Email para contato:".$_POST['p2']."
+        echo   </div>
         
         echo  <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
-        &nbsp;
-        </div>
+        echo &nbsp;
+        echo  </div>
         echo </body>
         echo</html>;
     $headers = 'From: '.$_POST['p2'].'\r\n'.'Content-type: text/html; charset=UTF-8';
