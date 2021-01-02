8f70ccbeccedfb6bbde0fe37a10bb45666f7d4b1
ABNER ANDRE <abnerndr@gmail.com>
2021-01-02 12:26:23 -0300

    teste

--- emailto.php
+++ emailto.php
@@ -1,29 +1,25 @@
 <?php
-
-    $to = 'orbt2001@gmail.com';
-    $subject = '[CONTATO] '.$_POST['p3'];
-    $html = "
-    <html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+// var_dump($_POST['p2']);
+$to      = 'orbt2001@gmail.com';
+$subject = '[CONTATO] '.$_POST['p3'];
+$html = "
+<html>
 <body>
 <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
-        <img src='https://orbt.com.br/orbt/img/orbtwh.svg' width='100'>
-    </div>
-
-  
-    
-    <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
-    &nbsp;
-    </div>
+	<img src='https://orbt.com.br/indesfer/img/logo.png' width='100'>
+</div>
+<div style='text-align:center; margin: 50px 10px;'>
+".$_POST['p1']." entrou em contato através do site: <br>".$_POST['p4']."
+<br> Email para contato:".$_POST['p2']."
+</div>
+<div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
+&nbsp;
+</div>
 </body>
 </html>";
-        
-$headers = "Content-type: text/html; charset=iso-8859-1";
+$headers = 'From: '.$_POST['p2'].'\r\n'.
+    'Content-type: text/html; charset=iso-8859-1rn'.
+    'X-Mailer: PHP/' . phpversion();
 
-    mail($to, $subject);
-    return true;
+mail($to, $subject, $html, $headers);
 ?>
