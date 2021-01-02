472c1924b7ccebafbdc549a343b926b888a8c571
ABNER ANDRE <abnerndr@gmail.com>
2021-01-02 12:37:43 -0300

    teste

--- emailto.php
+++ emailto.php
@@ -2,23 +2,20 @@
 // var_dump($_POST['p2']);
 $to = 'orbt2001@gmail.com';
 $subject = '[CONTATO] '.$_POST['p3'];
-$html = '
+$html = "
 <html>
 <body>
 <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
 	<img src='https://orbt.com.br/indesfer/img/logo.png' width='100'>
 </div>
 
-<div style='text-align:center; margin: 50px 10px;'>
-".$_POST['p1']." entrou em contato através do site: <br>".$_POST['p4']."
-<br> Email para contato:".$_POST['p2']."
-</div>
+
 
 <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
 &nbsp;
 </div>
 </body>
-</html>';
+</html>";
 $headers = 'From: '.$_POST['p2'].'\r\n'.
     'Content-type: text/html; charset=iso-8859-1rn'.
     'X-Mailer: PHP/' . phpversion();
