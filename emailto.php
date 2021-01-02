0a60286764cf85fc2fb1e6ff9f88368bd06edf52
ABNER ANDRE <abnerndr@gmail.com>
2021-01-02 12:36:14 -0300

    update

--- emailto.php
+++ emailto.php
@@ -23,6 +23,6 @@ $headers = 'From: '.$_POST['p2'].'\r\n'.
     'Content-type: text/html; charset=iso-8859-1rn'.
     'X-Mailer: PHP/' . phpversion();
 
-$result = mail($to, $subject, $html);
-$result = true;
+mail($to, $subject, $html);
+
 ?>
