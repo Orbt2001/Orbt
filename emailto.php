fa9731e576ee87a8d18e47460fa28d04f9a75c9c
ABNER ANDRE <abnerndr@gmail.com>
2021-01-02 12:11:27 -0300

    update unistall html

--- emailto.php
+++ emailto.php
@@ -3,20 +3,7 @@
 
     $to = 'orbt2001@gmail.com';
     $subject = '[CONTATO] '.$_POST['t3'];
-    $html = "<html>
-    <body>
-    <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
-    <img src='https://orbt.com.br/orbt/img/orbtwh.svg' width='100'>
-    </div>
-    
-   
-
-        
-    <div style='background-color:rgba(0,0,0,0.5); padding: 20px 20px;'>
-    &nbsp;
-    </div>
-    </body>
-    </html>";
+    $html = "";
         
     $headers='Content-Type: text/html; charset="utf-8"';
 
